7f4cc1deaf874f99c8518085ab0c6d437eab56a5
danny-vo <dannyphvo@gmail.com>
2018-09-16 16:52:17 -0500

    Fleshed out vector code

--- Vector.c
+++ Vector.c
@@ -6,11 +6,58 @@
 
 #define MINIMUM_SIZE 0
 
-Vector newVector(uint32_t initSize, void (*deletor)(void*)) {
-  Vector vector;
-  vector.length = initSize;
-  vector.capacity = initSize < MINIMUM_SIZE ? MINIMUM_SIZE : initSize;
-  vector.data = (void**)calloc(vector.capacity, sizeof(void*));
-  vector.deletor = deletor ? deletor : free;
+Vector* Vector_new(uint32_t initSize, void (*deletor)(void*), void (*printer)(void)) {
+  Vector* vector = (Vector*)malloc(sizeof(Vector));
+  vector->length = initSize;
+  vector->capacity = initSize < MINIMUM_SIZE ? MINIMUM_SIZE : initSize;
+  vector->data = (void**)calloc(vector->capacity, sizeof(void*));
+  vector->deletor = deletor ? deletor : free;
+  vector->printer = printer;
   return vector;
+}
+
+void Vector_destroy(Vector* vector) {
+  for (int i=0; i < vector->length; ++i) {
+    if (NULL == vector->data[i]) { continue; }
+    vector->deletor(vector->data[i]);
+  }
+  free(vector->data);
+  free(vector);
+}
+
+void* Vector_getElem(Vector* vector, uint32_t idx) {
+  return idx >= vector->length ? NULL : vector->data[idx];
+}
+
+int Vector_setElem(Vector* vector, uint32_t idx, void* elem) {
+  if (idx >= vector->length) {
+    return 0;
+  }
+  vector->data[idx] = elem;
+  return 1;
+}
+
+uint32_t Vector_size(Vector* vector) {
+  return vector->length;
+}
+
+void** Vector_getData(Vector* vector) {
+  return vector->data;
+}
+
+void* Vector_pop(Vector* vector) {
+  return vector->length <=0 ? NULL : vector->data[vector->length--];
+}
+
+void Vector_push(Vector* vector, void* elem) {
+  if (vector->length >= vector->capacity) {
+    vector->capacity *= 2;
+    void** newData = (char**)calloc(vector->capacity, sizeof(void*));
+    for (uint32_t i=0; i < vector->length; ++i) {
+      newData[i] = vector->data[i];
+    }
+    free(vector->data);
+    vector->data = newData;
+  }
+  vector->data[++(vector->length)] = elem;
 }
--- Vector.h
+++ Vector.h
@@ -8,6 +8,16 @@ typedef struct Vector {
   uint32_t length;
   uint32_t capacity;
   void (*deletor)(void*);
+  void (*printer)(void);
 } Vector;
+
+Vector* Vector_new(uint32_t initSize, void (*deletor)(void*), void (*printer)(void));
+void Vector_destroy(Vector* vector);
+void* Vector_getElem(Vector* vector, uint32_t idx);
+int Vector_setElem(Vector* vector, uint32_t idx, void* elem);
+uint32_t Vector_size(Vector* vector);
+void** Vector_getData(Vector* vector);
+void* Vector_pop(Vector* vector);
+void Vector_push(Vector* vector, void* elem);
 
 #endif /* VECTOR_H */
